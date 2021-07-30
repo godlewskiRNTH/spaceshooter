276bed4db30a43ac743e9a57f224d9274a2899ee
godlewskiRNTH <68201365+godlewskiRNTH@users.noreply.github.com>
2021-07-30 08:53:04 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+//  Background
+effects.starField.startScreenEffect()
+//  Spaceship
+let spaceship = sprites.create(img`
+    . . . . . . . c d . . . . . . .
+    . . . . . . . c d . . . . . . .
+    . . . . . . 2 c d 2 . . . . . .
+    . . . . . 2 2 c b 2 2 . . . . .
+    . . . . 2 2 2 f f 2 2 2 . . . .
+    . . . . . . . c 2 . . . . . . .
+    . . . . . . . f f . . . . . . .
+    . . . . . . . e 2 . . . . . . .
+    . . . . . . e e 4 e . . . . . .
+    . . . . . . e 2 4 e . . . . . .
+    . . . . . c c c e e e . . . . .
+    . . . . e e 2 2 2 4 e e . . . .
+    . . c f f f c c e e f f e e . .
+    . c c c c e e 2 2 2 2 4 2 e e .
+    c c c c c c e e 2 2 2 4 2 2 e e
+    c c c c c c e e 2 2 2 2 4 2 e e
+`)
